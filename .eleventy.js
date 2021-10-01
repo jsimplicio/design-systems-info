3b4fdf67789725a5bbfad862d7fa4fb73ebd91a1
jsimplicio <hi@julessimplic.io>
2021-10-01 15:04:54 -0500

    Fix list style, add quote.

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,9 @@
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('css')
+  eleventyConfig.addPassthroughCopy('firefox')
   eleventyConfig.addPassthroughCopy('favicon')
+
   return {
     passthroughFileCopy: true
   }
